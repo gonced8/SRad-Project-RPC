73e7dcdf5941d44883a314e64e0f3cc6f7082591
gonced8 <gonced8@gmail.com>
2019-11-18 17:10:02 +0000

    simple pulse plot

--- rpc.py
+++ rpc.py
@@ -1,4 +1,37 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-if __name__ == '__main':
-    pass
+if __name__ == "__main__":
+
+    f = 10      # [Hz]
+    tau = 1     # [s]
+
+    Ts = np.min([1/f, tau])/10     # Nyquist theorem is /2
+
+    tspan = [0, 7]
+
+    t = np.arange(tspan[0], tspan[1], Ts)
+
+    A = 1
+    s = np.empty(t.shape)
+
+    for i, ti in enumerate(t):
+        if ti>0 and ti<tau:
+            s[i] = A*np.cos(2*np.pi*f*ti)
+        else:
+            s[i] = 0
+
+    plt.figure()
+    plt.plot(t, s)
+    plt.xlabel("time [s]")
+    plt.ylabel("amplitude")
+    plt.title("Simple Pulse")
+
+
+
+
+
+
+
+
+    plt.show()    
